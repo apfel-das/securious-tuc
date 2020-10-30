bd7c9002b2be00b5e47e77b78a2f67046552d82e
Konstantinos Pantelis <kpantelis@isc.tuc.gr>
2020-10-30 16:34:25 +0200

    This thing seems cool
    -Keygen. Encrypt and Decrypt are ready..
    -Utility functions also included.
    -Still stuff tbdone.

--- encryptor/src/assign_1.c
+++ encryptor/src/assign_1.c
@@ -11,18 +11,25 @@
 #define BLOCK_SIZE 16
 
 
-/* function prototypes */
+/* Utility function prototypes */
+
+
 void print_hex(unsigned char *, size_t);
 void print_string(unsigned char *, size_t); 
 void usage(void);
 void check_args(char *, char *, unsigned char *, int, int);
+unsigned char *readFile(char *fPath, unsigned long *len);
+void writeFile(char *fPath, unsigned char *data, unsigned long *len);
+
+/* Actual encryption functions */
+
 void keygen(unsigned char *, unsigned char *, unsigned char *, int);
-void encrypt(unsigned char *, int, unsigned char *, unsigned char *, 
-    unsigned char *, int );
-int decrypt(unsigned char *, int, unsigned char *, unsigned char *, 
-    unsigned char *, int);
+void encrypt(unsigned char *, int, unsigned char *, unsigned char *, unsigned char *, int );
+int decrypt(unsigned char *, int, unsigned char *, unsigned char *, unsigned char *, int);
 void gen_cmac(unsigned char *, size_t, unsigned char *, unsigned char *, int);
 int verify_cmac(unsigned char *, unsigned char *);
+
+
 
 
 
@@ -30,12 +37,16 @@ int verify_cmac(unsigned char *, unsigned char *);
 
 
 
+/* Variables */
+
+unsigned char globalKey[256];
+unsigned char iv[256];
+
 /*
  * Prints the hex value of the input
  * 16 values per line
  */
-void
-print_hex(unsigned char *data, size_t len)
+void print_hex(unsigned char *data, size_t len)
 {
 	size_t i;
 
@@ -55,8 +66,7 @@ print_hex(unsigned char *data, size_t len)
 /*
  * Prints the input as string
  */
-void
-print_string(unsigned char *data, size_t len)
+void print_string(unsigned char *data, size_t len)
 {
 	size_t i;
 
@@ -74,8 +84,7 @@ print_string(unsigned char *data, size_t len)
  * Prints the usage message
  * Describe the usage of the new arguments you introduce
  */
-void
-usage(void)
+void usage(void)
 {
 	printf(
 	    "\n"
@@ -105,9 +114,7 @@ usage(void)
  * Checks the validity of the arguments
  * Check the new arguments you introduce
  */
-void
-check_args(char *input_file, char *output_file, unsigned char *password, 
-    int bit_mode, int op_mode)
+void check_args(char *input_file, char *output_file, unsigned char *password,  int bit_mode, int op_mode)
 {
 	if (!input_file) {
 		printf("Error: No input file!\n");
@@ -137,14 +144,39 @@ check_args(char *input_file, char *output_file, unsigned char *password,
 
 
 /*
- * Generates a key using a password
+ * Generates a key using a given password.
+   Arguments:	
+   					<char *password>: A password under which the key gets generated.
+   					<char *key>: 	  Points the location in which the generated key gets to be stored.
+   					<char *iv>:
+   					<char *bit_mode>: The actual bits of the AES-ECB to be produced.
+   	Notes: 
+   					- <char *bit_mode> is guaranteed to be 128 or 256.
+					- sha1 hashing is applied.
+					- no "salt" is used.
  */
-void
-keygen(unsigned char *password, unsigned char *key, unsigned char *iv,
-    int bit_mode)
+void keygen(unsigned char *password, unsigned char *key, unsigned char *iv, int bit_mode)
 {
 
-	/* TODO Task A */
+
+
+	//defining parameters.
+	const unsigned char *salt = NULL;
+	const EVP_CIPHER *cipher;
+	const EVP_MD *hash = EVP_get_digestbyname("sha1");
+
+
+	//getting the cipher in respect to the bit mode.
+	(bit_mode == 128) ? (cipher = EVP_get_cipherbyname("aes-128-ecb")): (cipher = EVP_get_cipherbyname("aes-256-ecb"));
+
+	//generating key and error handling.
+
+	if (EVP_BytesToKey(cipher, hash, salt, (unsigned char *)password, strlen((char *)password), 1, globalKey, iv) == 0)
+	{
+		fprintf(stderr, "Error in key generation..\n");
+		exit(-1);
+	}
+	
 
 }
 
@@ -152,12 +184,59 @@ keygen(unsigned char *password, unsigned char *key, unsigned char *iv,
 /*
  * Encrypts the data
  */
-void
-encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-    unsigned char *iv, unsigned char *ciphertext, int bit_mode)
+void encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext, int bit_mode)
 {
 
-	/* TODO Task B */
+	printf("%s\n", plaintext);
+	//parameters
+	EVP_CIPHER_CTX *context; 
+	const EVP_CIPHER *cipher;
+	int encryptionLength;
+
+	//init. the context.
+	context =  EVP_CIPHER_CTX_new();
+
+	//getting the cipher with respect on the bit mode.
+	(bit_mode == 128) ? (cipher = EVP_get_cipherbyname("aes-128-ecb")): (cipher = EVP_get_cipherbyname("aes-256-ecb"));
+
+	//be brave, apply assertion. [remove if feeling sure about the results]
+	OPENSSL_assert(EVP_CIPHER_CTX_key_length(context) == (bit_mode % 8));
+
+
+	//init. encryption process and error handling.
+	if(EVP_EncryptInit_ex(context, cipher, NULL, key, iv) == 0)
+	{
+		fprintf(stderr, "Error upon context initialization [Encryption]\n");
+		exit(-1);
+	}
+
+
+	//updating the encryption, error handling as well.
+
+	if(!EVP_EncryptUpdate(context, ciphertext, &encryptionLength, plaintext, plaintext_len))
+	{
+		EVP_CIPHER_CTX_free(context);
+		fprintf(stderr, "Error upon updating context [Encryption]\n");
+		exit(-1);
+	}
+
+
+	//finalising encryption process with error handling as always.
+	if(!EVP_EncryptFinal_ex(context, ciphertext + encryptionLength, &encryptionLength))
+	{
+		EVP_CIPHER_CTX_free(context);
+		fprintf(stderr, "Error upon finalising the encryption process [Encryption]\n");
+		exit(-1);
+
+
+	}
+
+	//releasing sensitive info from memory.
+	EVP_CIPHER_CTX_free(context);
+
+	printf("Done Encrypting..\n");
+	print_hex(ciphertext, (size_t)encryptionLength);
+	print_string(ciphertext, (size_t)encryptionLength);
 
 }
 
@@ -165,15 +244,67 @@ encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
 /*
  * Decrypts the data and returns the plaintext size
  */
-int
-decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-    unsigned char *iv, unsigned char *plaintext, int bit_mode)
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext, int bit_mode)
 {
-	int plaintext_len;
+	int plaintext_len = 0;
+	
+	//parameters
+	EVP_CIPHER_CTX *context; 
+	const EVP_CIPHER *cipher;
+	int encryptionLength;
 
-	plaintext_len = 0;
+	//init. the context.
+	context =  EVP_CIPHER_CTX_new();
 
-	/*TODO Task C */
+	//getting the cipher with respect on the bit mode.
+	(bit_mode == 128) ? (cipher = EVP_get_cipherbyname("aes-128-ecb")): (cipher = EVP_get_cipherbyname("aes-256-ecb"));
+
+	//be brave, apply assertion. [remove if feeling sure about the results]
+	OPENSSL_assert(EVP_CIPHER_CTX_key_length(context) == (bit_mode % 8));
+
+
+	//init. encryption process and error handling.
+	if(EVP_DecryptInit_ex(context, cipher, NULL, key, iv) == 0)
+	{
+		fprintf(stderr, "Error upon context initialization [Decryption]\n");
+		exit(-1);
+	}
+
+
+	//updating the encryption, error handling as well.
+
+	if(!EVP_DecryptUpdate(context, plaintext, &encryptionLength, ciphertext, ciphertext_len))
+	{
+		EVP_CIPHER_CTX_free(context);
+		fprintf(stderr, "Error upon updating context [Decryption]\n");
+		exit(-1);
+	}
+
+	//update count.
+	plaintext_len = encryptionLength;
+
+	//finalising encryption process with error handling as always.
+	if(!EVP_DecryptFinal_ex(context, plaintext + encryptionLength, &encryptionLength))
+	{
+		EVP_CIPHER_CTX_free(context);
+		fprintf(stderr, "Error upon finalising the encryption process [Decryption]\n");
+		exit(-1);
+
+
+	}
+	//finalize length.
+	plaintext_len += encryptionLength;
+
+	//releasing sensitive info from memory.
+	EVP_CIPHER_CTX_free(context);
+
+	printf("Done Decrypting..\n");
+	print_hex(plaintext, (size_t)encryptionLength);
+	print_string(plaintext, (size_t)plaintext_len);
+	printf("%s\n", plaintext);
+	
+
+
 
 	return plaintext_len;
 }
@@ -209,6 +340,83 @@ verify_cmac(unsigned char *cmac1, unsigned char *cmac2)
 
 
 
+/* Utility functions */
+
+
+
+/*
+	Parses and reads the content of a file, specified by <char *fPath>.
+	Arguments:
+				<char *fPath>:			full path of the file to be read.
+				<unsigned long *len>: 	pointer to a location, whre the size of data in bytes will be stored.
+	Returns:
+				Data read as an <unsigned char *> [a form of string in C].
+*/
+
+unsigned char *readFile(char *fPath, unsigned long *len)
+{
+	FILE *fp;
+	unsigned char *data;
+	size_t retrieved = 0;
+
+	//open the file, via the file pointer.
+
+	fp  = fopen(fPath, "rb");
+	
+	//error handling on dummy file location.
+	if(!fp)
+	{
+		fprintf(stderr, "File not found, tool will terminate now..\n" );
+		exit(-1);
+	}
+	
+	//get the length of the file.
+	fseek(fp, 0, SEEK_END);
+	*len = ftell(fp);
+
+	//re-calibrate, goto start.
+	fseek(fp, 0, SEEK_SET);
+
+	//allocate some space.
+	data  = (unsigned char *)malloc(*len);
+
+	//some error handling and then read.
+	if(data)
+		retrieved = fread(data, 1, *len, fp);
+
+	if(!retrieved)
+		return NULL;
+	return data;
+
+
+}
+
+
+/*
+	
+	Writes data in an output file.
+
+*/
+void writeFile(char *fPath, unsigned char *data, unsigned long *len)
+{
+
+	//initialise a file pointer;
+	FILE *fp;
+
+	fp = fopen(fPath, "wb");
+
+	if(!fp)
+	{
+		fprintf(stderr, "File not found, tool will terminate now..\n" );
+		exit(-1);
+	}
+
+	fwrite(data, 1 ,(size_t)len, fp);
+
+}
+
+
+
 /* TODO Develop your functions here... */
 
 
@@ -224,8 +432,7 @@ verify_cmac(unsigned char *cmac1, unsigned char *cmac2)
  * Decrypts and verifies the input file and stores the plaintext to the output
  * file
  */
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int opt;			/* used for command line arguments */
 	int bit_mode;			/* defines the key-size 128 or 256 */
@@ -233,6 +440,19 @@ main(int argc, char **argv)
 	char *input_file;		/* path to the input file */
 	char *output_file;		/* path to the output file */
 	unsigned char *password;	/* the user defined password */
+
+	//usefull when holding input, output.
+
+	unsigned char *input = NULL;
+	unsigned char *output = NULL;
+	unsigned long inpLen = 0;
+	unsigned long outpLen = 0;
+
+
+
+
+
+
 
 	/* Init arguments */
 	input_file = NULL;
@@ -285,20 +505,71 @@ main(int argc, char **argv)
 	/* check arguments */
 	check_args(input_file, output_file, password, bit_mode, op_mode);
 
+	
 
 
-	/* TODO Develop the logic of your tool here... */
+	/// Free the input buffer, then do stuff.
+
+	if(input_file != NULL)
+		input= readFile(input_file, &inpLen);
+	if(!input)
+	{
+		fprintf(stderr, "Empty data set given, exiting now..\n");
+		exit(-1);
+	}
+	
 
 
+	/*
 
-
-	/* Initialize the library */
+	Initialize the library, no need to be done in version above OpenSSL 1.1.0
+	Uncomment if running versions below 1.1.0
+	
+	*/
+	//OpenSSL_add_all_algorithms(); 
 
 
 	/* Keygen from password */
 
+	printf("Input:\t%s\n", input);
 
+	printf("Password:%s\n", password);
+	keygen(password, globalKey, iv, bit_mode);
+
+	printf("Key:%s Length[bits]: %ld \n", globalKey, strlen((const char *)globalKey)*8);
 	/* Operate on the data according to the mode */
+
+	switch(op_mode)
+	{
+		case 0:
+
+				printf("About to encrypt..[Debug]\n");
+
+				//strlen(output) = strlen(input) + strlen(padding) + strlen(CMACM)
+				//allocate appropriate space.
+
+				outpLen = inpLen - (inpLen % BLOCK_SIZE) + BLOCK_SIZE;
+				output = (unsigned char *)malloc(outpLen);
+
+				//encrypt..
+				encrypt(input, inpLen, globalKey, iv, output, bit_mode);
+
+				printf("Decrypting instantly..[Debug]\n");
+				outpLen = decrypt(output, outpLen, globalKey, NULL, input, bit_mode);
+
+				break;
+		default:
+				fprintf(stderr, "Invalid opt_mode..\n");	
+				break;
+	}
+
+
+
+
+
+
+
+
 	/* encrypt */
 
 	/* decrypt */
@@ -312,6 +583,9 @@ main(int argc, char **argv)
 	free(input_file);
 	free(output_file);
 	free(password);
+
+	free(input);
+	free(output);
 
 
 	/* END */
