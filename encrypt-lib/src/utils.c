a863327fab77e343a0ba5d2f69ba8786b1d0a681
Konstantinos Pantelis <kpantelis@isc.tuc.gr>
2020-11-09 21:20:18 +0200

    I/O seems fine.
    - Added function for reading/writing files.

--- encrypt-lib/src/utils.c
+++ encrypt-lib/src/utils.c
@@ -1,4 +1,7 @@
 #include "utils.h"
+#ifndef BUF_SIZ
+#define BUF_SIZ 512
+#endif
 
 /*
  * Prints the hex value of the input
@@ -103,5 +106,141 @@ void check_args(char *input_file, char *output_file, char *key_file, int op_mode
 		usage();
 	}
 }
+
+/*
+	Parses and reads the content of a file, specified by <char *fPath>.
+	Arguments:
+				<char *fPath>:			full path of the file to be read.
+				<unsigned long *len>: 	pointer to a location, whre the size of data in bytes will be stored.
+	Returns:
+				Data read as an <unsigned char *> [a form of string in C].
+*/
+
+unsigned char *readFile(char *fPath, unsigned long *len)
+{
+	FILE *fp;
+	long fileLen = 0;
+	
+
+	//allocate some space on a buffer.
+	unsigned char *data = (unsigned char *)malloc(sizeof(unsigned char )*BUF_SIZ + 1);
+
+
+	//open the file, via the file pointer.
+
+	fp  = fopen(fPath, "rb");
+	
+	//error handling on dummy file location.
+	if(fp == NULL)
+	{
+		fprintf(stderr, "Error on opening file, tool will terminate now..\n" );
+		exit(EXIT_FAILURE);
+	}
+
+	//estimate the length.
+	fseek(fp, 0, SEEK_END);
+
+	//unsafe yet useful for our cause.
+	fileLen = ftell(fp);
+
+	//set on beginning of file.
+	rewind(fp);
+
+	//read the first character.
+	char ch = getc(fp);
+	int charCount = 0;
+
+
+	/*
+
+		Checking for EOF [ch != EOF] will work on normal ASCII files.
+		For reading the encrypted this won't work cause encrypted chars might contain EOF [0xffff] under some OS's.
+		So it is prefered to check for file length via the estimation of ftell() even if ftell() is partially unsafe [especially under Windows].
+
+	*/
+	while(charCount != fileLen)
+	{
+		
+		//re-allocate if needed.
+		if(charCount >= BUF_SIZ)
+			data = realloc(data, (charCount + 1)*sizeof(unsigned char));
+
+		//counter incrementation and storage.
+		data[charCount] = ch;
+		charCount++;
+
+		//read next char.
+		ch = getc(fp);
+
+
+
+	}
+	
+	//some error handling and then read.
+	if(!data)
+	{
+		fprintf(stderr, "Error on initializing memory, tool will terminate now..\n" );
+		exit(EXIT_FAILURE);
+	}
+
+
+
+	//append termination
+	*(data + charCount) = '\0';
+
+	//inform about the size
+	*len = charCount;
+
+	//avoid leaks.
+	fclose(fp);
+
+
+	return data;
+
+
+
+}
+
+
+
+
+
+/*
+	
+	Writes data in an output file.
+
+	Arguments:
+				<char *fPath>:			full path of the file to be filled.
+				<unsigned long *len>: 	pointer to a location, where the size of data in bytes will be retrieved.
+	Returns:
+				None.
+	Note:
+				<unsigned long len>: should not be NULL, should have a valid length.
+
+*/
+void writeFile(char *fPath, unsigned char *data, unsigned long len)
+{
+
+	//initialise a file pointer;
+	FILE *fp;
+	
+
+	// opt will determine whether bytes or ASCII will be written.
+
+	fp = fopen(fPath, "wb");
+
+	if(!fp)
+	{
+		fprintf(stderr, "File not found, tool will terminate now..\n" );
+		exit(-1);
+	}
+
+	///write as a stream and not character wise.
+	fwrite(data, sizeof(unsigned char) ,(size_t)len, fp);
+
+	fclose(fp);
+}
+
+
 
 
--- encrypt-lib/src/utils.h
+++ encrypt-lib/src/utils.h
@@ -21,15 +21,13 @@ print_hex(unsigned char *, size_t);
  * arg0: data
  * arg1: data len
  */
-void
-print_string(unsigned char *, size_t);
+void print_string(unsigned char *, size_t);
 
 
 /*
  * Prints the usage message
  */
-void
-usage(void);
+void usage(void);
 
 
 /*
@@ -40,8 +38,38 @@ usage(void);
  * arg2: path to key file
  * arg3: operation mode
  */
-void
-check_args(char *, char *, char *, int);
+void check_args(char *, char *, char *, int);
+
+
+/*
+
+	Reads a file's content.
+
+	arg1: path to input file.
+	arg2: file length.
+	Note: <arg2> will be calculated inside function, therefore it is suggested to be passed as NULL.
+
+
+*/
+
+unsigned char *readFile(char *fPath, unsigned long *len);
+
+
+/*
+	Writes content on a file.
+
+	arg1: path to output file.
+	arg2: pointer on content to be written as an <unsigned char *>.
+	arg3: length of content.
+	
+	Note: <arg3> should not be NULL, it is suggested to be strlen(const char *arg3) or something similar for non-'\0' terminated content.
+
+
+*/
+
+void writeFile(char *fPath, unsigned char *data, unsigned long len);
 
 
 #endif /* _UTILS_H */
+
+
